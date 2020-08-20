015a5cf6806ea520624bfc33d45d57cbbe89d402
walid younis <walid.younis.90@gmail.com>
2020-08-20 20:26:58 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -9,7 +9,7 @@ def reverse_each_word(string)
     .join(" ")
 end
 
-def reverse_each_word_collect(string)
+def reverse_each_word(string)
     array = string.split(" ")
   new_array = array.collect do 
     |element|
